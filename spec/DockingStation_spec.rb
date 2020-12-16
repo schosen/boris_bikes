5cecb6ee89cd826fcc753b83faf07f305f255f56
Sarah <sarahchosen@hotmail.co.uk>
2020-12-16 12:25:19 +0000

    completed chapter 20

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -1,18 +1,17 @@
 require 'DockingStation'
 
 describe DockingStation do 
+  let(:bike) { double :bike }
   it { is_expected.to respond_to :release_bike }
 
   it 'docks something' do
-    bike = Bike.new
-
     expect(subject.dock(bike)).to include(bike) #changed from eq (bike) to --> include(bike) so that would pass part 14
   end 
 
-  it 'returns docked bikes' do
-    bike = Bike.new
-    subject.dock(bike)
+  it { is_expected.to respond_to(:dock).with(1).argument }
 
+  it 'returns docked bikes' do
+    subject.dock(bike)
     expect(subject.bike).to eq bike
   end 
 
@@ -25,28 +24,31 @@ describe DockingStation do
   # not sure if above is needed?
 
   describe ' #release_bike' do
+    let(:bike) { double :bike }
     it 'raises an error when there are no working bikes available' do
-      # arrange -> add new station and bike
-      bike = Bike.new
       # act -> report bike as broken then dock bike
-      bike.report_broken
+      allow(bike).to receive(:broken?).and_return(true)
+      # bike = double(:bike, broken?: true) <-- shorthand syntax if you had stub for bike being bike = double(:bike)
       subject.dock(bike)
       expect { subject.release_bike }.to raise_error 'No bikes available'
     end
 
+    it 'raises an error when there are no bikes available' do
+      expect { subject.release_bike }.to raise_error 'No bikes available'
+    end
   end
 
   describe '#dock' do
     it 'raises an error when full' do
-      subject.capacity.times { subject.dock Bike.new } 
-      expect { subject.dock Bike.new }.to raise_error 'Docking station full'
+      subject.capacity.times { subject.dock double :bike } 
+      expect { subject.dock double(:bike) }.to raise_error 'Docking station full'
     end
   end
 
   describe 'initialization' do
 
     subject { DockingStation.new }
-    let(:bike) { Bike.new }
+    let(:bike) { double :bike }
     it 'defaults capacity' do
       described_class::DEFAULT_CAPACITY.times do
         subject.dock(bike)
